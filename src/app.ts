6c56094a7fe813be171b6697ca294d66839b316d
Ikarosv <ikarovieiraa@gmail.com>
2023-04-28 16:46:16 -0300

    ✨ feat: adiciona rota users

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import products from './controllers/products';
+import users from './controllers/users';
 
 const app = express();
 
 app.use(express.json());
 
 app.use('/products', products);
+app.use('/users', users);
 
 export default app;
