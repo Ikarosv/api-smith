24a5e783996d439207bb72113fc33938b371fa7c
Ikarosv <ikarovieiraa@gmail.com>
2023-04-28 17:10:25 -0300

    🐛 fix: adicina stringfy no payload do token

--- src/services/users.ts
+++ src/services/users.ts
@@ -7,10 +7,10 @@ const JWT_SECRET = process.env.JWT_SECRET || 'secret';
 
 export const createUserService = async (user: User): Promise<string> => {
   const userCreated = await createUser(user);
-
+  
   if (!userCreated) errorGenerator(500, 'Internal Server Error');
 
-  return sign(userCreated, JWT_SECRET);
+  return sign(JSON.stringify(userCreated), JWT_SECRET);
 };
 
 export const getUserService = async () => {};
